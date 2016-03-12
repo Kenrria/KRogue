5218140edef672aefaf3ed2a8b61c67f1f0136eb
Jesús Cruz <cruzoliverajesus@gmail.com>
2016-03-12 23:35:42 +0100

    Added working doors and walls

--- moveCurses.c
+++ moveCurses.c
@@ -1,5 +1,8 @@
 
 /* A very small game, it's just a character '@' moving around the terminal within a map */
+/* '.' represents an empty cell
+/* '#' represents a wall (you cannot walk through it)
+/* '+' represents a door (use it to enter rooms)
 /* Press 'q' for exit, and 'w','s','a','d' for moving */
 
 #include <curses.h> 
@@ -24,15 +27,18 @@ int main() {
 	clear();	
 	refresh();	
 	
-
+	nRows = mapRows -1 ;
+	nCols = mapColumns -1 ;
 	posX = 0; posY = 0;
-	printMap(mapWalls1Ascii, mapRows, mapColumns);	// draw the initial map
+	printMap(mapWallsDoorsAscii, mapRows, mapColumns);	// draw the initial map
 	move(posX,posY);
+	mvdelch(posY,posX);
 	insch('@');
+	refresh();
 	while (1){					// Start the game, wait for player imput
 		d = getch();			
 		if (d == 'q') break;	// Exit!
-		printMap(mapWalls1Ascii, mapRows, mapColumns);
+		printMap(mapWallsDoorsAscii, mapRows, mapColumns);
 		draw(d);				// Try to move the character
 		move(posY,posX);
 	}
@@ -46,57 +52,45 @@ void draw(char d){
 	if (checkMovement(d)) {
 		if ( d == 'd') {			// Move Right
 			posX++;
-			mvdelch(posY,posX);		// Delete the previous '@'
-			move(posY,posX);
-			insch('@');	
-			refresh(); 
 		}
 		if ( d == 'a') {			// Move Lef
 			posX--;
-			mvdelch(posY,posX);
-			move(posY,posX);
-			insch('@');	
-			refresh(); 
 		}
 		if ( d == 'w') {			// Move Up
 			posY--;
-			mvdelch(posY,posX);
-			move(posY,posX);
-			insch('@');	
-			refresh(); 
 		}
 		if ( d == 's') {			// Move Down
 			posY++;
-			mvdelch(posY,posX);
-			move(posY,posX);
-			insch('@');	
-			refresh(); 
 		}
 	}
+	mvdelch(posY,posX);
+	move(posY,posX);
+	insch('@');	
+	refresh(); 
 }
 
 /* Check window boundaries*/
 int checkMovement(char d){
 	if ( d == 'd'){					// Check right
-		if (posX+1 > nCols){
+		if (posX+1 > nCols || mapWallsDoorsAscii[posY][posX+1] ==35 ){
 			return 0;
 		}
 		return 1;
 	}
 	if ( d == 'a'){					// Check left
-		if (posX-1 < 0){
+		if (posX-1 < 0 || mapWallsDoorsAscii[posY][posX-1] ==35 ){
 			return 0;
 		}
 		return 1;
 	}
 	if ( d == 'w'){					// Check  Up
-		if (posY-1 < 0){
+		if (posY-1 < 0 || mapWallsDoorsAscii[posY-1][posX] ==35 ){
 			return 0;
 		}
 		return 1;
 	}
 	if ( d == 's'){					// Check Down
-		if ( posY+1 > nRows){
+		if ( posY+1 > nRows || mapWallsDoorsAscii[posY+1][posX] ==35 ){
 			return 0;
 		}
 		return 1;
